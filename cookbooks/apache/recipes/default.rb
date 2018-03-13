79a77c16328ab7d62e48329be1d35a6f1f319998
Michael Mueller <user@mmueller891.mylabserver.com>
2018-03-13 16:59:09 +0000

    Added If statement in default.rb

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,6 +4,12 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #install apache
+if node['platform_family'] =="rhel"
+	package ='httpd'
+elsif node['platform_family'] =="debian"
+	package ='apache2'
+end
+
 package 'apache2' do
 	package_name 'httpd'
 	action :upgrade
